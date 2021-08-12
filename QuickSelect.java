8f67cf4ef5a85dc5bbb512ef7e16ba6b581c330a
rohansharma0 <iamrohan29@gmail.com>
2021-08-13 00:45:35 +0530

    D04-Quick Select

---
+++ QuickSelect.java
@@ -0,0 +1,61 @@
+import java.util.Arrays;
+
+public class QuickSelect {
+
+    public static void main(String[] args) {
+
+        int arr[] = {64, 34, 25, 12, 22, 11, 90};
+
+        //place smallest element
+        int k = 4;
+
+        // index = 4 -1 ;
+        int i = 4 -1;
+        System.out.println(quickSelect(arr , 0 , arr.length -1 , i));
+
+
+    }
+
+    public static int quickSelect(int[] arr, int lo, int hi, int k) {
+
+        int p = partition(arr , arr[hi] , lo  , hi);
+
+        if(p == k){
+            return arr[k];
+        }else if(p > k){
+
+            return quickSelect(arr , lo , p- 1 , k);
+
+        }else{
+            return quickSelect(arr , p +1 , hi , k);
+        }
+
+
+    }
+
+    public static int partition(int[] arr, int pivot, int lo, int hi) {
+
+        int i = lo;
+        int j = lo;
+
+        while(i <= hi){
+
+            if(arr[i] > pivot){
+                i++;
+            }else {
+                //swap;
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+
+                i++;
+                j++;
+
+            }
+
+        }
+
+        return j -1;
+
+    }
+}
