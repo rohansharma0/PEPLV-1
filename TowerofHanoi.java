880c0e1b93a9324b98e40f0d326ddc941e9da453
rohansharma0 <iamrohan29@gmail.com>
2021-08-14 13:05:46 +0530

    D05-Tower of Hanoi

---
+++ TowerofHanoi.java
@@ -0,0 +1,23 @@
+public class TowerofHanoi {
+
+    public static void main(String[] args) {
+
+        toh(3 , 10 , 11, 12);
+    }
+
+    public static void toh(int n, int t1id, int t2id, int t3id){
+
+        if(n == 0){
+            return;
+        }
+
+        toh(n -1 , t1id , t3id , t2id);
+
+        System.out.println(n + "[" + t1id + "->" + t2id + "]" );
+
+        toh(n - 1, t3id , t2id, t1id);
+
+
+    }
+
+}
