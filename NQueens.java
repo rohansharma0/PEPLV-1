1e8c0bc19b77b8d56fe5624b7b9192b2929e0b9a
rohansharma0 <iamrohan29@gmail.com>
2021-08-14 13:05:20 +0530

    D05-NQueens

---
+++ NQueens.java
@@ -0,0 +1,75 @@
+import java.util.*;
+
+public class NQueens {
+
+    public static List<List<String>> res = new ArrayList<>();
+
+
+    public static void main(String[] args) {
+
+        int n = 1;
+        char[][] board = new char[n][n];
+
+        for(int i = 0 ; i< n ; i++){
+            for(int j = 0 ; j< n; j++){
+                board[i][j] = '.';
+            }
+        }
+
+        solveNQueens(board, 0 , n);
+
+        System.out.println(res);
+    }
+
+    public static void solveNQueens(char[][] board , int row , int n) {
+
+
+        if(row == n){
+            res.add(construct(board));
+            return;
+        }
+
+        for(int col = 0 ; col<n ; col++){
+
+            if(isSafeForQueen(board, row, col)){
+                board[row][col] = 'Q';
+                solveNQueens(board, row + 1 , n);
+                board[row][col] = '.';
+            }
+
+        }
+
+    }
+
+
+    public static boolean isSafeForQueen(char[][] chess , int row , int col){
+
+        for(int i = row -1; i>= 0 ; i--){
+            if(chess[i][col] == 'Q'){
+                return false;
+            }
+        }
+        for(int i = row - 1, j = col -1 ; i>= 0 && j>=0 ; i-- , j--){
+            if(chess[i][j] == 'Q'){
+                return false;
+            }
+        }
+        for(int i = row - 1, j = col +1 ; i>= 0 && j< chess.length ; i-- , j++){
+            if(chess[i][j] == 'Q'){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static List<String> construct(char [][] board){
+        List<String> res = new ArrayList<>();
+
+        for(int i = 0 ;i< board.length ; i++){
+            String s = new String(board[i]);
+            res.add(s);
+        }
+        return  res;
+    }
+
+}
